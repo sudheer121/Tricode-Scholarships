cb6c6deadda603519fd6b494266f67b8e28a9922
Sudheer121 <sudheer121@gmail.com>
2020-11-20 23:36:38 +0530

    Added sequalize migration

--- server/index.js
+++ server/index.js
@@ -1,17 +1,26 @@
 //jshint esversion:6
 
 require('dotenv').config();
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
-require("./startup/dbconfig")();
+const sequelize =  require("./startup/dbconfig")();
 // var cookieParser = require("cookie-parser");
 // app.use(cookieParser());
 
 const register = require("./routes/register");
 app.use("/", register);
 
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Server running on post 3000");
+app.listen(process.env.PORT || 7000, function () {
+  console.log("Server running on post 7000");
+  testConnection();
 });
--- server/startup/dbconfig.js
+++ server/startup/dbconfig.js
@@ -1,13 +1,6 @@
-import Sequelize from 'sequelize';
+const { Sequelize } = require('sequelize');
 
 module.exports = function () {
     const sequelize = new Sequelize('mysql://root:password@localhost:5001/tricode');
-    async function testConnection() {
-        try {
-            await sequelize.authenticate();
-            console.log('Connection has been established successfully.');
-        } catch (error) {
-            console.error('Unable to connect to the database:', error);
-        }
-    }
+    return sequelize;
 };
