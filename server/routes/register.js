faf65421812cd44d63e5269b44663c4b0aa93352
Sudheer121 <sudheer121@gmail.com>
2020-11-21 00:47:52 +0530

    {backend] Migrating User

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,35 +1,23 @@
-const {
-    getUserByEmail,
-    create 
-} = require("../models/User");
+const User = require("../models/user");
 
 const { genSaltSync, hashSync } = require('bcryptjs');
 
 const express = require("express");
 const router = express.Router();
 
-router.post("/register",async (req,res)=>{
-    const body = req.body; 
-    const salt = genSaltSync(10);
-    body.password = hashSync(body.password, salt);  
+router.use(express.json());
 
+router.post("/register", async (req, res) => {
+    const body = req.body;
+    const salt = genSaltSync(10);
+    console.log(body);
+    body.password = hashSync(body.password, salt);
     try {
-        const result = await getUserByEmail(body.email);
-        if(result.length) { 
-            throw "email already exists"
-        } else {
-            const created = await create(body);
-            res.json({
-                success:1,
-                message:"User created"
-            }); 
-        }
-    } catch(error) {
-        
-        res.json({
-            success:0,
-            message:error 
-        }); 
+        let newUser = await User.findAll({ where: { email: body.email } });
+        console.log(newUser.toJSON());
+        return newUser.toJSON();
+    } catch (error) {
+        console.log(error);
     }
 
 });
