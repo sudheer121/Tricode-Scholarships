473672d0e27bdf377b4ea7f96e4569c7d1f80365
Sudheer Tripathi <tripathisudheer604@gmail.com>
2020-11-21 13:04:23 +0530

    add student table

---
+++ server/models/student.js
@@ -0,0 +1,30 @@
+const { Model } = require('sequelize');
+module.exports = (sequelize, Sequelize) => {
+  class Student extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Student.init({
+    user_id: Sequelize.INTEGER, 
+    name: Sequelize.STRING, 
+    father_name: Sequelize.STRING,
+    mother_name: Sequelize.STRING,
+    last_name: Sequelize.STRING,
+    mobile_num: Sequelize.STRING, 
+    college_name: Sequelize.STRING, 
+    course : Sequelize.STRING,
+    latest_marks : Sequelize.STRING, 
+    yearly_family_income :Sequelize.STRING,
+
+  }, {
+    sequelize,
+    modelName: 'Student',
+  });
+  return User;
+};
