8fff57f99d52416d65fa6ee8e740d893686728c1
Sudheer Tripathi <tripathisudheer604@gmail.com>
2020-11-21 15:24:29 +0530

    add cors middleware

--- server/index.js
+++ server/index.js
@@ -8,6 +8,15 @@ const {db} = require("./models")
 
 // var cookieParser = require("cookie-parser");
 // app.use(cookieParser());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+});
 
 const register = require("./routes/register");
 const login = require("./routes/login");
